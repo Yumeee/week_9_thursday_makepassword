2917d7da410c100b89031ddecb5436811e2b5e39
YUMEE <yumeehk@hotmail.com>
2018-07-13 14:30:46 -0400

    passwords work now

--- makepassword.rb
+++ makepassword.rb
@@ -1,8 +1,9 @@
 require 'HTTParty'
+require 'pry'
 
-res = HTTParty.get("https://makemeapassword.org/api/v1/pasphrase/json?pc=10&wc=8&s=RandomForever")
-
+res = HTTParty.get("https://makemeapassword.org/api/v1/alphanumeric/json?c=10&l=6")
 password_response = JSON.parse(res.body)
+binding.pry
 
 passwords = []
 password_response['pws'].each do |password|
@@ -10,3 +11,5 @@ password_response['pws'].each do |password|
     passwords << password
   end
 end
+
+puts passwords
